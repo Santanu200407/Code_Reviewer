12bdd589df5a6d1ed7531416dcc38e81d2a26835
Santanu Mondal <santanumondal4168@gmail.com>
2025-11-19 04:16:47 +0530

    origin changed

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -5,6 +5,13 @@ const app=express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
 app.get("/",(req,res)=>{
     res.send("Hello world")
 })
